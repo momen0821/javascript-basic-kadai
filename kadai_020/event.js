6a0637bb11edc8e927c79abf260e76eee8d5905d
momen0821 <168943804+momen0821@users.noreply.github.com>
2024-06-29 14:44:05 +0900

    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const addBtn = document.getElementById('btn');
+const parentElement=document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+    parentElement.textContent='ボタンをクリックしました';
+});
